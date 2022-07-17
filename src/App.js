2f277ade901843da8f77949adee78dda759f3b61
KuuLotus <kuulotusy@gmail.com>
2022-07-18 03:42:26 +0900

    useRef,useReducer,useMemo

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
-import { useEffect, useState, useContext } from "react";
+import { useEffect, useState, useContext, useRef, useReducer, useMemo} from "react";
 import NaritaRyoContext from ".";
 import "./App.css";
+
+const reducer = (state, action) => {
+  switch (action.type) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    default:
+      return state;
+  }
+};
 
 function App() {
   const [count, setCount] = useState(0);
   const naritaryoInfo = useContext(NaritaRyoContext);
+  const ref = useRef();
+  const [state, dispatch] = useReducer(reducer, 0);
 
   const handleClick = () => {
     setCount(count + 1);
@@ -13,6 +26,31 @@ function App() {
   useEffect(() => {
     console.log("Hello Hooks");
   }, [count]);
+
+  const handleRef = () => {
+    console.log(ref.current.value);
+  };
+
+  // useMemo
+  const [count01, setCount01] = useState(0);
+  const [count02, setCount02] = useState(0);
+
+  // const square = () => {
+  //   let i = 0;
+  //   while (i < 20000000000) {
+  //     i++;
+  //   }
+  //   return count02 * count02;
+  // };
+
+  const square = useMemo(() => {
+    let i = 0;
+    // 重い処理
+    while (i < 20000) {
+      i++;
+    }
+    return count02 * count02;
+  }, [count02]);
 
   return (
     <div className="App">
@@ -24,8 +62,30 @@ function App() {
       <h1>useContext</h1>
       <p>{naritaryoInfo.name}</p>
       <p>{naritaryoInfo.age}</p>
+
+      <hr />
+      <h1>useRef</h1>
+      <input type="text" ref={ref} />
+      <button onClick={handleRef}>UseRef</button>
+
+      <hr />
+      <h1>useReducer</h1>
+      <p>カウント：{state}</p>
+      <button onClick={() => dispatch({ type: "increment" })}>＋</button>
+      <button onClick={() => dispatch({ type: "decrement" })}>ー</button>
+
+      <hr />
+      <h1>useMemo</h1>
+      <div>カウント①： {count01}</div>
+      <div>カウント②： {count02}</div>
+      <div>結果：{square}</div>
+      <button onClick={() => setCount01(count01 + 1)}>＋</button>
+      <button onClick={() => setCount02(count02 + 1)}>＋</button>
+
+      <hr />
+      <h1>useCallBack</h1>
     </div>
-  );
+  )
 }
 
 export default App;
